6994bf3cd6177f6a045349323f0797dc05a816c1
PRANJAY GULERIA <pranjayguleria@gmail.com>
2023-05-05 15:10:14 +0530

    Updates

---
+++ precache-manifest.2d9eaaac9e8eb61ca3ae9f40b12d767e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38e7c45abfab0d088d9c",
+    "url": "/TV-SHOWS-website/static/css/main.6ab5e9cf.chunk.css"
+  },
+  {
+    "revision": "38e7c45abfab0d088d9c",
+    "url": "/TV-SHOWS-website/static/js/main.38e7c45a.chunk.js"
+  },
+  {
+    "revision": "508f3280b16fef6e9375",
+    "url": "/TV-SHOWS-website/static/js/1.508f3280.chunk.js"
+  },
+  {
+    "revision": "6d1b1fb41d37f4bc41d6",
+    "url": "/TV-SHOWS-website/static/js/runtime~main.6d1b1fb4.js"
+  },
+  {
+    "revision": "43b4525b0f51bb8aab48e13e43a36e86",
+    "url": "/TV-SHOWS-website/index.html"
+  }
+];
